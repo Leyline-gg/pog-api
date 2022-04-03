6ce34fe97c458eff876d569e42ef05afa5d372b6
Kharaone <kharaone@gmail.com>
2022-04-03 12:02:20 +0200

    refactor: linting category and oracle acceptance

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: '@loopback/eslint-config',
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn', // or error
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
